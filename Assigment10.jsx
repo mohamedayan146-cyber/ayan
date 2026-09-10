1046bb0c91b17e9b3b3ee1d7dad447516d80bd2c
mohamedayan146-cyber <mohamedayan146@gmail.com>
2026-09-10 07:32:13 +0300

    Add files via upload

---
+++ Assigment10.jsx
@@ -0,0 +1,25 @@
+import {UserList} from "./UserList"
+const UserList = ({ users }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserList;
